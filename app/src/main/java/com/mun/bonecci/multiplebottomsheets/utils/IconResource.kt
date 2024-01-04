6d826a651c57dfa08f73c8d9d08f4c1b26c7fa20
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2024-01-04 11:51:37 -0600

    docs added to IconResource.kt

--- app/src/main/java/com/mun/bonecci/multiplebottomsheets/utils/IconResource.kt
+++ app/src/main/java/com/mun/bonecci/multiplebottomsheets/utils/IconResource.kt
@@ -7,11 +7,22 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 
+/**
+ * Utility class for creating [Painter] instances from either a drawable resource ID or an [ImageVector].
+ *
+ * @property resID Drawable resource ID.
+ * @property imageVector Image vector to be used as an alternative to drawable resource.
+ */
 class IconResource private constructor(
     @DrawableRes private val resID: Int?,
     private val imageVector: ImageVector?
 ) {
 
+    /**
+     * Converts the [IconResource] to a [Painter] resource.
+     *
+     * @return [Painter] resource based on the drawable resource ID or image vector.
+     */
     @Composable
     fun asPainterResource(): Painter {
         resID?.let {
@@ -21,10 +32,22 @@ class IconResource private constructor(
     }
 
     companion object {
+        /**
+         * Creates an [IconResource] instance from a drawable resource ID.
+         *
+         * @param resID Drawable resource ID.
+         * @return [IconResource] instance with the specified drawable resource ID.
+         */
         fun fromDrawableResource(@DrawableRes resID: Int): IconResource {
             return IconResource(resID, null)
         }
 
+        /**
+         * Creates an [IconResource] instance from an [ImageVector].
+         *
+         * @param imageVector Image vector to be used as an alternative to drawable resource.
+         * @return [IconResource] instance with the specified image vector.
+         */
         fun fromImageVector(imageVector: ImageVector?): IconResource {
             return IconResource(null, imageVector)
         }
